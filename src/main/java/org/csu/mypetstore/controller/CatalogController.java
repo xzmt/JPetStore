4a3f89c8dac801ab4134b5ddccf1795b314f34ab
xzmt <xzmt_kz@hotmail.com>
2020-04-05 10:01:11 +0800

    合并乔松分支

--- src/main/java/org/csu/mypetstore/controller/CatalogController.java
+++ src/main/java/org/csu/mypetstore/controller/CatalogController.java
@@ -70,6 +70,45 @@ public class CatalogController {
         return "catalog/Item";
     }
 
+    @RequestMapping("/ajaxSearch")
+    public String ajaxSearch(HttpServletRequest request, HttpServletResponse response)
+    {
+
+
+        response.setContentType("text/html;charset=utf-8");//解决乱码
+        String keyword = request.getParameter("term");//默认传过来的参数名成为term，封装了文本框中输入的值
+        System.out.println(keyword);
+        StringBuffer sb = new StringBuffer("[");
+        try
+        {
+            List<String> names = catalogService.searchAllProductList(keyword);
+            for (int i = 0; i < names.size(); i++)
+            {
+                if (i == names.size() - 1)
+                {
+                    //注意在拼接的时候，要用双引号，单引号我试过，不起作用好像是,至少key必须是双引号，最好都写成双引号
+                    /*
+                     * 其中label属性用于显示在autocomplete弹出菜单，而value属性则是选中后给文本框赋的值。如果没有指定其中一个属性
+                     * 则用另一个属性替代(即value和label值一样)，如果label和value都没有指定，则无法用于autocomplete的提示。
+                     */
+                    sb.append("{\"lable\":\"" + names.get(i) + "\",\"value\":\"" + names.get(i) + "\"}]");
+                }
+                else
+                {
+                    sb.append("{\"lable\":\"" + names.get(i) + "\",\"value\":\"" + names.get(i) + "\"},");
+                }
+            }
+
+            System.out.println(names.size());
+            response.getWriter().write(sb.toString());
+        }
+        catch (Exception e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
+    }
     @RequestMapping("/ajaxWindow")
     public void ajaxWindow(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String categoryId = request.getParameter("description");
